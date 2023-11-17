2d8d7ee955582b023af8684873423e39f091ade2
Nifra <102341577+NifraWahaj@users.noreply.github.com>
2023-11-18 00:42:53 +0500

    Update Logout.java

--- src/BusinessLayer/Logout.java
+++ src/BusinessLayer/Logout.java
@@ -1,11 +1,6 @@
 package BusinessLayer;
-
 public class Logout {
     public static void logOut(User user) {
         user.setLoggedIn(false);
-    }
-    public static void logOutAdmin(Admin admin) {
-        admin.setLoggedIn(false);
-        System.out.println("Admin logged out");
     }
 }
