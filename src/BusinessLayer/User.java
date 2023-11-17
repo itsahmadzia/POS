18820401e913b1ed5fab24bbcecbd1df1bce275b
Nifra <102341577+NifraWahaj@users.noreply.github.com>
2023-11-18 00:39:32 +0500

    Update User.java

--- src/BusinessLayer/User.java
+++ src/BusinessLayer/User.java
@@ -1,41 +1,39 @@
 package BusinessLayer;
-
 import java.util.ArrayList;
 
 public class User {
-    private int id;
-    private String name;
+    private String username;
     private String password;
-    private Role role;
+    private Role role; 
     protected boolean loggedIn = false;
 
-    public User(int id, String name, String password,Role role) {
-        this.id = id;
-        this.name = name;
+    public User(String username, String password, Role role) {
+        this.username = username;
         this.password = password;
         this.role = role;
     }
-    
-    public boolean login(String inputName, String inputPassword) {
-        return name.equals(inputName) && password.equals(inputPassword);
+
+    public boolean login(String inputUsername, String inputPassword) {
+        return username.equals(inputUsername) && password.equals(inputPassword);
     }
-    
+
     public boolean isLoggedIn() {
         return loggedIn;
     }
-    
+
     public void save(ArrayList<User> users) {
         users.add(this);
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    void setLoggedIn(boolean b) {
+    public void setLoggedIn(boolean b) {
         loggedIn = b;
     }
-       public Role getRole() {
+
+    public Role getRole() {
         return role;
     }
 }
