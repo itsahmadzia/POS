cb470d61ad790c774ff45f2307f7284c75a4e835
ahmad zia <mahmad2004a8@gmail.com>
2023-11-20 12:48:26 +0500

    k

--- src/UILayer/AdminUI.java
+++ src/UILayer/AdminUI.java
@@ -310,6 +310,7 @@ public class AdminUI extends javax.swing.JFrame {
         if (dialogResult == JOptionPane.YES_OPTION) {
             setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
             this.dispose();
+
         }
     }                                             
 
