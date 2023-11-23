70b6019999eb83330d141c38c51bc678b3d92c34
Nifra <102341577+NifraWahaj@users.noreply.github.com>
2023-11-23 06:03:46 +0500

    Update Role.java

--- src/BusinessLayer/Role.java
+++ src/BusinessLayer/Role.java
@@ -1,10 +1,11 @@
 package BusinessLayer;
 
 public abstract class Role {
-    protected String log; // assistant/manager
+   //protected String log; // assistant/manager
     public abstract void permissions();
 
-    public String getLog() {
+    /*public String getLog() {
         return log;
-    }
+    }*/
+    public abstract String getRoleType();
 }
