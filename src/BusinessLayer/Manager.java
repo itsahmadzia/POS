6d71fc37885bba279fdad0c61ede6212c440cd8b
Nifra <102341577+NifraWahaj@users.noreply.github.com>
2023-11-23 06:03:27 +0500

    Update Manager.java

--- src/BusinessLayer/Manager.java
+++ src/BusinessLayer/Manager.java
@@ -1,28 +1,12 @@
 package BusinessLayer;
-import DBLayer.ManagerDAO;
 
 public class Manager extends Role {
-    private boolean LoggedIn;
-    private String name;
-    private String password;
-    
-    public Manager(String name, String password) {
-        this.name = name;
-        this.password = password;
-    }
-    public boolean isLoggedIn() {
-        return LoggedIn;
-    }
-    
-    public void setLoggedIn(boolean b) {
-        LoggedIn = b;
-    }
     @Override
     public void permissions() {
         System.out.println("Manager has access to manager-specific functionality");
     }
     
-    public boolean authenticateFromDB(String username, String password){
-        return ManagerDAO.authenticate(this, username, password);
+    public String getRoleType() {
+        return "Manager";
     }
 }
