86df07426703f3e5ce466e2378a842b49bdc2c77
Nifra <102341577+NifraWahaj@users.noreply.github.com>
2023-11-23 06:04:09 +0500

    Update SalesAssistant.java

--- src/BusinessLayer/SalesAssistant.java
+++ src/BusinessLayer/SalesAssistant.java
@@ -1,27 +1,7 @@
 package BusinessLayer;
-import DBLayer.SalesAssistantDAO;
 
 public class SalesAssistant extends Role {
-    private boolean LoggedIn;
-    private String name;
-    private String password;
-    
-    public SalesAssistant(String name, String password) {
-        this.name = name;
-        this.password = password;
-    }
-    public boolean isLoggedIn() {
-        return LoggedIn;
-    }
-    
-    public void setLoggedIn(boolean b) {
-        LoggedIn = b;
-    }
-        
-    public boolean authenticateFromDB(String username, String password){
-        return SalesAssistantDAO.authenticate(this, username, password);
-    }
-   
+     
     @Override
     public void permissions() {
         System.out.println("Sales Assistant granted access to sales assistant-specific functionality");
@@ -29,5 +9,9 @@ public class SalesAssistant extends Role {
 
     public void processOrder() {
        
+    }
+    @Override
+    public String getRoleType() {
+        return "Operator"; //since table in db is operator
     }
 }
