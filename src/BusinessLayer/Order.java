9be2f6b78dbb82c23fef25fd5a8e38cf454bf1e6
Nifra <102341577+NifraWahaj@users.noreply.github.com>
2023-11-25 18:26:58 +0500

    Update Order.java

--- src/BusinessLayer/Order.java
+++ src/BusinessLayer/Order.java
@@ -1,12 +1,30 @@
 package BusinessLayer;
-
+import DBLayer.OrderDAO;
 import java.util.Date;
+import java.util.List;
 
-public class Order extends ItemContainer {
+public class Order extends ItemContainer{
     private int order_id;
     private String customerName;
+    private String operatorName;
     private double total;
     Date timestamp ;
+    OrderDAO OrderDAO;
+    private double amountPaid;
+    
+    public Order(String customerName, Date timestamp, List<Item> items,double total,double amountPaid,String operatorName) {
+        this.customerName = customerName;
+        this.timestamp = timestamp;
+        this.total = total;
+        this.amountPaid = amountPaid;
+        this.operatorName = operatorName;
+        this.items.addAll(items);
+        this.OrderDAO = new OrderDAO();
+    }
+    
+    public Order(){
+        this.OrderDAO = new OrderDAO();
+    }
 
     public void setOrder_id(int order_id) {
         this.order_id = order_id;
@@ -48,4 +66,28 @@ public class Order extends ItemContainer {
         text.append("               Total ").append(total) ;
         return text.toString();
     }
+    
+    public int saveOrder(Order order) {
+        if (OrderDAO != null) {
+            order_id = OrderDAO.saveOrder(order);
+        } else {
+            System.out.println("OrderDAO is null. Cannot save order.");
+        }
+        return order_id;
+    }
+
+
+    public double getAmountPaid(){
+        return amountPaid;
+    }
+    public double getTotal(){
+        return total;
+    }
+    public String getOperatorName() {
+        return operatorName;
+    }
+    public void updateStockInDB(int orderId, String productName, int quantityOrdered) {
+         OrderDAO.updateStock( orderId,  productName,  quantityOrdered);
+    }
+
 }
