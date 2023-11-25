aa8d6de451781e288cbb7b40ea911ce3d2733ecb
Nifra <102341577+NifraWahaj@users.noreply.github.com>
2023-11-25 18:24:45 +0500

    Update ItemContainer.java

--- src/BusinessLayer/ItemContainer.java
+++ src/BusinessLayer/ItemContainer.java
@@ -1,22 +1,34 @@
 package BusinessLayer;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public  class ItemContainer {
-protected int ID;
-protected List<Item> items = new ArrayList<>();
+    protected int ID;
+    public List<Item> items = new ArrayList<>();
 
-
-public void  add(Item i){
-items.add(i);
-}
-public double total_price(){
-    double sum=0;
-    for(Item i : items){
-      sum+=  i.getPrice();
+    public void add(Item i) {
+        items.add(i);
     }
-    return  sum;
-}
+
+    public double total_price(){
+        double sum=0;
+        for(Item i : items){
+          sum+=  i.getPrice();
+        }
+        return  sum;
+    }
+    
+    public void  remove(Item i){
+        items.remove(i);
+    } 
+    
+    public Item findItemById(int productId) {
+        for (Item item : items) {
+            if (item.getProduct().getId() == productId) {
+                return item;
+            }
+        }
+        return null;
+    }
 
 }
