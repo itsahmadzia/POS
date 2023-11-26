bee9ff8b73bae952119fcea0a20c7557e1550e88
Nifra <102341577+NifraWahaj@users.noreply.github.com>
2023-11-26 20:37:24 +0500

    removed unused function permissions

--- src/BusinessLayer/Manager.java
+++ src/BusinessLayer/Manager.java
@@ -1,11 +1,7 @@
 package BusinessLayer;
 
 public class Manager extends Role {
-    @Override
-    public void permissions() {
-        System.out.println("Manager has access to manager-specific functionality");
-    }
-    
+
     public String getRoleType() {
         return "Manager";
     }
