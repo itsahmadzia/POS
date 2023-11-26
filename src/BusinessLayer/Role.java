d31be78014b4097e116320297aa0f1a41f92d9e0
Nifra <102341577+NifraWahaj@users.noreply.github.com>
2023-11-26 20:39:11 +0500

    removed unused function permissions

--- src/BusinessLayer/Role.java
+++ src/BusinessLayer/Role.java
@@ -1,11 +1,6 @@
 package BusinessLayer;
 
 public abstract class Role {
-   //protected String log; // assistant/manager
-    public abstract void permissions();
 
-    /*public String getLog() {
-        return log;
-    }*/
     public abstract String getRoleType();
 }
