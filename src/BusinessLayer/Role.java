d800a1cf677eddb8948c7eedd1f57a813330b614
Nifra <102341577+NifraWahaj@users.noreply.github.com>
2023-11-30 08:17:28 +0500

    javadoc added

--- src/BusinessLayer/Role.java
+++ src/BusinessLayer/Role.java
@@ -1,6 +1,13 @@
 package BusinessLayer;
-
+/**
+ * The Role class is an abstract class representing a role in the system.
+ * Subclasses must implement the getRoleType method to provide the specific role type.
+ */
 public abstract class Role {
-
+    /**
+     * Gets the type of the role.
+     *
+     * @return The type of the role.
+     */
     public abstract String getRoleType();
 }
