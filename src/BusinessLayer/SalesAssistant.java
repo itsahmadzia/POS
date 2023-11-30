53370e1a91364c805d634bf412f02d4c41b58998
Nifra <102341577+NifraWahaj@users.noreply.github.com>
2023-11-30 08:17:59 +0500

    javadoc added

--- src/BusinessLayer/SalesAssistant.java
+++ src/BusinessLayer/SalesAssistant.java
@@ -2,8 +2,16 @@ package BusinessLayer;
 
 import DBLayer.OrderDAO;
 import DBLayer.ProductDAO;
-
+/**
+ * The SalesAssistant class represents the role of a sales assistant (also referred to as an operator) in the system.
+ * It extends the Role class and implements the getRoleType method to return the role type "Operator."
+ */
 public class SalesAssistant extends Role {
+    /**
+     * Gets the type of the role, which is "Operator" for a sales assistant.
+     *
+     * @return The type of the role ("Operator" for a sales assistant).
+     */
     @Override
     public String getRoleType() {
         return "Operator"; //since table in db is operator
