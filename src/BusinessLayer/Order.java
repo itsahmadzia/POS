cd9f0222c74cd7f78de426156fa7892fa37b2933
Nifra <102341577+NifraWahaj@users.noreply.github.com>
2023-11-30 08:16:13 +0500

    javadoc added

--- src/BusinessLayer/Order.java
+++ src/BusinessLayer/Order.java
@@ -2,7 +2,10 @@ package BusinessLayer;
 import DBLayer.OrderDAO;
 import java.util.Date;
 import java.util.List;
-
+/**
+ * The Order class represents a customer order containing a collection of items.
+ * It extends the ItemContainer class and provides methods to manage and the process order.
+ */
 public class Order extends ItemContainer{
     private int order_id;
     private String customerName;
@@ -12,6 +15,17 @@ public class Order extends ItemContainer{
     OrderDAO OrderDAO;
     private double amountPaid;
     
+    /**
+     * Constructs a new Order with the specified customer information, timestamp, items, total amount,
+     * amount paid, and operator name.
+     *
+     * @param customerName The name of the customer placing the order.
+     * @param timestamp    The timestamp indicating when the order was placed.
+     * @param items        The list of items included in the order.
+     * @param total        The total amount of the order.
+     * @param amountPaid   The amount paid by the customer.
+     * @param operatorName The name of the operator/sales assistant processing the order.
+     */
     public Order(String customerName, Date timestamp, List<Item> items,double total,double amountPaid,String operatorName) {
         this.customerName = customerName;
         this.timestamp = timestamp;
@@ -21,37 +35,72 @@ public class Order extends ItemContainer{
         this.items.addAll(items);
         this.OrderDAO = new OrderDAO();
     }
-    
+    /**
+     * Constructs an empty Order.
+     */
     public Order(){
         this.OrderDAO = new OrderDAO();
     }
-
+    /**
+     * Sets the order ID.
+     *
+     * @param order_id The unique id for the order.
+     */
     public void setOrder_id(int order_id) {
         this.order_id = order_id;
     }
+    /**
+     * Sets the name of the customer.
+     *
+     * @param customerName The name of the customer.
+     */
 
     public void setCustomerName(String customerName) {
         this.customerName = customerName;
     }
-
+    
+    /**
+     * Gets the order ID.
+     *
+     * @return The unique id for the order.
+     */
     public int getOrder_id() {
         return order_id;
     }
-
+    /**
+    * Returns the customer name associated with this order.
+    *
+    * @return The customer name.
+    */
     public String getCustomerName() {
         return customerName;
     }
-
+    /**
+     * Sets the timestamp of the order.
+     *
+     * @param timestamp The timestamp indicating when the order was placed.
+     */
     public void setTimestamp(Date timestamp) {
         this.timestamp = timestamp;
     }
-
+    /**
+     * Gets the timestamp of the order.
+     *
+     * @return The timestamp indicating when the order was placed.
+     */
     public Date getTimestamp() {
         return timestamp;
     }
+    /*
     public void cancel(){
 
-    }
+    }*/
+    
+    /**
+     * Generates an invoice for the order, including details of items, quantities, and total amount.
+     *
+     * @return The generated invoice as a string.
+     */
     public String generateInvoice(){
         double sum=0;
         StringBuilder text= new StringBuilder();
@@ -66,6 +115,12 @@ public class Order extends ItemContainer{
         text.append("               Total ").append(total) ;
         return text.toString();
     }
+     /**
+     * Saves the order to the database using the OrderDAO.
+     *
+     * @param order The order to be saved.
+     * @return The order ID assigned to the saved order.
+     */
     
     public int saveOrder(Order order) {
         if (OrderDAO != null) {
@@ -76,18 +131,47 @@ public class Order extends ItemContainer{
         return order_id;
     }
 
-
+    /**
+     * Gets the amount paid by the customer.
+     *
+     * @return The amount paid by the customer.
+     */
     public double getAmountPaid(){
         return amountPaid;
     }
+     /**
+     * Gets the total amount of the order.
+     *
+     * @return The total amount of the order.
+     */
     public double getTotal(){
         return total;
     }
+    /**
+     * Gets the name of the operator processing the order.
+     *
+     * @return The name of the operator.
+     */
     public String getOperatorName() {
         return operatorName;
     }
+    /**
+     * Updates the stock in the database for the specified order details.
+     *
+     * @param orderId         The ID of the order.
+     * @param productName     The name of the product.
+     * @param quantityOrdered The quantity ordered.
+     */
     public void updateStockInDB(int orderId, String productName, int quantityOrdered) {
          OrderDAO.updateStock( orderId,  productName,  quantityOrdered);
+    }
+    /**
+     * Sets the name of the operator processing the order.
+     *
+     * @param operatorName The name of the operator.
+     */
+    public void setOperatorName(String operatorName) {
+        this.operatorName = operatorName;
     }
 
 }
