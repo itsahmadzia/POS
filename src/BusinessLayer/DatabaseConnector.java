004fc77f0ad1cf5ed99124d0c8a948f9c753aed4
Nifra <102341577+NifraWahaj@users.noreply.github.com>
2023-11-30 08:12:50 +0500

    javadoc added

--- src/BusinessLayer/DatabaseConnector.java
+++ src/BusinessLayer/DatabaseConnector.java
@@ -3,19 +3,30 @@ package BusinessLayer;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+/**
+ * The DatabaseConnector class is responsible for establishing and managing the
+ * connection to the database.
+ */
 public class DatabaseConnector {
     private Connection connection;
     private String url;
     private String username;
     private String password;
 
+     /**
+     * Constructs a new DatabaseConnector with default database connection
+     * parameters.
+     */
     public DatabaseConnector() {
         this.url ="jdbc:mysql://localhost:3306/randomn";
         this.username = "ostechnix";
         this.password = "Password123#@!";
     }
-
+    /**
+     * Establishes a connection to the database.
+     *
+     * @return The Connection object representing the database connection.
+     */
     public Connection connect() {
         try {
             if (connection == null || connection.isClosed()) {
@@ -29,7 +40,9 @@ public class DatabaseConnector {
         }
         return connection;
     }
-
+    /**
+     * Disconnects from the database.
+     */
     public void disconnect() {
         if (connection != null) {
             try {
