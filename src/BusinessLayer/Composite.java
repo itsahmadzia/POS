da341fdfb27889760727851f54edccb65561a6f0
Nifra <102341577+NifraWahaj@users.noreply.github.com>
2023-11-30 08:11:44 +0500

    added javadoc

--- src/BusinessLayer/Composite.java
+++ src/BusinessLayer/Composite.java
@@ -1,5 +1,12 @@
 package BusinessLayer;
 
+/**
+ * The Composite interface represents the common interface for all components
+ * in the Composite pattern. It declares a method for displaying the component.
+ */
 public interface Composite {
+    /**
+     * Displays information about the component.
+     */
     void display();
 }
