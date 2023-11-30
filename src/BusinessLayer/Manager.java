4945fef9410ecbf971a334f98afee06f1cca1246
Nifra <102341577+NifraWahaj@users.noreply.github.com>
2023-11-30 08:15:02 +0500

    javadoc added

--- src/BusinessLayer/Manager.java
+++ src/BusinessLayer/Manager.java
@@ -1,7 +1,15 @@
 package BusinessLayer;
-
+/**
+ * The Manager class represents a user in the system with the role type Manager.
+ * This class extends the abstract Role class, providing a specific implementation
+ * for the getRoleType method.
+ */
 public class Manager extends Role {
-
+    /**
+     * Gets the role type of the Manager, which is "Manager".
+     *
+     * @return The role type of the Manager.
+     */
     public String getRoleType() {
         return "Manager";
     }
