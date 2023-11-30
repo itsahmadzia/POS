5c0024b306716f3a4ab6fb958445a9659a22629e
Nifra <102341577+NifraWahaj@users.noreply.github.com>
2023-11-30 08:14:35 +0500

    javadoc added

--- src/BusinessLayer/Logout.java
+++ src/BusinessLayer/Logout.java
@@ -1,11 +1,22 @@
 package BusinessLayer;
 
+/**
+ * The Logout class provides methods for logging out users and administrator.
+ */
 public class Logout {
-
+/**
+     * Logs out a user by setting their logged-in status to false.
+     *
+     * @param user The user to be logged out.
+     */
     public static void logOut(User user) {
         user.setLoggedIn(false);
     }
-
+    /**
+     * Logs out an administrator by setting their logged-in status to false.
+     *
+     * @param admin The administrator to be logged out.
+     */
     public static void logOutAdmin(Admin admin) {
         admin.setLoggedIn(false);
     }
